812d24c1bc6c3cf9182d793c89640bc2ae5ce7ad
amiraliamhh <amiraliamhh@gmail.com>
2020-05-22 20:00:19 +0430

    feat: rotate on drag

--- 3dbox/script.js
+++ 3dbox/script.js
@@ -1,43 +1,19 @@
-const actions = document.querySelector('#actions')
-actions.addEventListener('click', e => {
-  const box = document.querySelector('#box')
-  box.className = ''
-  box.classList.add(`show-${e.target.name}`)
-})
-
 const box = document.querySelector('#box')
 let dragging = false
 let oldX = null
 let oldY = null
-box.addEventListener('mousedown', e => {
+document.addEventListener('mousedown', e => {
   dragging = true
 })
-box.addEventListener('mouseup', e => {
+document.addEventListener('mouseup', e => {
   dragging = false
 })
-box.addEventListener('mouseleave', e => {
+document.addEventListener('mouseleave', e => {
   dragging = false
 })
-box.addEventListener('mousemove', e => {
+document.addEventListener('mousemove', e => {
   if (dragging) {
-    if (oldX === null) {
-      oldX = e.pageX
-    } else {
-      if (oldX - e.pageX > 0) {
-        pushY(box)
-      } else if (oldX - e.pageX < 0) {
-        pushY(box, 'right')
-      }
-    }
-    if (oldY === null) {
-      oldY = e.pageY
-    } else {
-      if (oldY - e.pageY > 0) {
-        pushX(box)
-      } else if (oldY - e.pageY < 0) {
-        pushX(box, 'bottom')
-      }
-    }
+    rotate(box, [oldY - e.pageY, oldX - e.pageX])
     oldX = e.pageX
     oldY = e.pageY
   }
@@ -46,38 +22,21 @@ box.addEventListener('mousemove', e => {
 const matchRotateYValue = /(?<=rotateY\()(.*)(?=\))/
 const matchRotateXValue = /(?<=rotateX\()(.*)(?=\))/
 
-function pushY(el, direction = 'left') {
-  const rotationY = getRotation(el)[1]
-  let newRotationY = 0
-  if (direction === 'left') {
-    newRotationY = rotationY - 1
-  } else {
-    newRotationY = rotationY + 1
+function rotate(el, [x , y] = [0, 0]) {
+  const rotations = getRotation(el)
+  let newRotationX = rotations[0]
+  let newRotationY = rotations[1]
+  if (x > 0) {
+    newRotationX = rotations[0] - 1
+  } else if (x < 0) {
+    newRotationX = rotations[0] + 1
   }
-  if (!el.style.transform) {
-    el.style.transform = `rotateY(${newRotationY}deg)`
-    return
+  if (y > 0) {
+    newRotationY = rotations[1] - 1
+  } else if (y < 0) {
+    newRotationY = rotations[1] + 1
   }
-  setTimeout(() => {
-    el.style.transform = el.style.transform.replace(matchRotateYValue, `${newRotationY}deg`)
-  }, 0);
-}
-
-function pushX(el, direction = 'top') {
-  const rotationX = getRotation(el)[0]
-  let newRotationX = 0
-  if (direction === 'top') {
-    newRotationX = rotationX - 1
-  } else {
-    newRotationX = rotationX + 1
-  }
-  if (!el.style.transform) {
-    el.style.transform = `rotateX(${newRotationX}deg)`
-    return
-  }
-  setTimeout(() => {
-    el.style.transform = el.style.transform.replace(matchRotateXValue, `${newRotationX}deg`)
-  }, 0);
+  el.style.transform = `rotateX(${newRotationX}deg) rotateY(${newRotationY}deg)`
 }
 
 function getRotation(el) {
@@ -85,33 +44,19 @@ function getRotation(el) {
   if (!tr) {
     return [0, 0]
   }
-  return [getRotateX(tr), getRotateY(tr)]
+  return [getRotationVal(tr, matchRotateXValue), getRotationVal(tr, matchRotateYValue)]
 }
 
-function getRotateY(str = '') {
-  let rotateY = str.match(matchRotateYValue)
-  if (rotateY) {
-    rotateY = rotateY[0]
+function getRotationVal(str = '', re = matchRotateYValue) {
+  let rotate = str.match(re)
+  if (rotate) {
+    rotate = rotate[0]
   } else {
     return 0
   }
-  rotateY = parseInt(rotateY.replace(/\D/g,''), 0)
+  rotate = parseInt(rotate.replace(/\D/g,''), 0)
   if (str.indexOf('-') > -1) {
-    return rotateY * -1
+    return rotate * -1
   }
-  return rotateY
-}
-
-function getRotateX(str = '') {
-  let rotateX = str.match(matchRotateXValue)
-  if (rotateX) {
-    rotateX = rotateX[0]
-  } else {
-    return 0
-  }
-  rotateX = parseInt(rotateX.replace(/\D/g,''), 0)
-  if (str.indexOf('-') > -1) {
-    return rotateX * -1
-  }
-  return rotateX
+  return rotate
 }
